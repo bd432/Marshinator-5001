03fa679539df0c24fb7ef44e7a36ae323f5438df
bd432 <77358799+bd432@users.noreply.github.com>
2021-10-27 17:37:07 +0100

    Changes before pull

--- read_serial.py
+++ read_serial.py
@@ -11,9 +11,11 @@ ser = serial.Serial("COM10", 9600)
 print("Start program")
 
     
+
 # Parameters
 x_len = 50         # Number of points to display
 y_range = [0, 50]  # Range of possible Y values to display
+y_range = [0, 30]  # Range of possible Y values to display
 # Create figure for plotting
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
